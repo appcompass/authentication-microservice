e222c5e08b7e0a9f663081faf74019a576440a29
Jubair Saidi <jubair.saidi@gmail.com>
2021-06-23 10:51:35 -0400

    renames auth module to api to be more descriptive

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from './api/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
 import { MessagingModule } from './messaging/messaging.module';
 
